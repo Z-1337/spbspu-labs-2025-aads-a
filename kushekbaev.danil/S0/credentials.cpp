a1f749527b7f6d042f26d1b2c6e3423d7771eae9
Z-1337 <un228der@gmail.com>
2025-05-16 18:10:05 +0300

    Revert "kushekbaev.danil/S0"

--- kushekbaev.danil/S0/credentials.cpp
+++
@@ -1,7 +0,0 @@
-#include "credentials.hpp"
-#include <iostream>
-
-std::ostream & kushekbaev::outCredentials(std::ostream & out)
-{
-  return out << "kushekbaev.danil";
-}
--- kushekbaev.danil/S0/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "credentials.hpp"
-
-int main()
-{
-  kushekbaev::outCredentials(std::cout) << "\n";
-}
